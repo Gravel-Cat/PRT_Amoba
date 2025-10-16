a22bf3a57439e1809e6cc92ef6a5f22ff6251f68
Juhász Benedek <juhaszbeni51@gmail.com>
2025-10-16 12:27:35 +0200

    Added move reading functionality

--- src/main/java/hu/nye/progtech/service/MoveHandler.java
+++ src/main/java/hu/nye/progtech/service/MoveHandler.java
@@ -1,7 +1,29 @@
 package hu.nye.progtech.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Scanner;
+
 public class MoveHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MoveHandler.class);
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void setMove(char[][] moves, char move, int row, int column) {
         moves[(row - 1)][(column - 1)] = move;
+    }
+
+    public static void readMove(char[][] moves, int mapSize) {
+        int row;
+        int column;
+        LOGGER.info("Please enter your move's placement (row, column):");
+        row = scanner.nextInt();
+        column = scanner.next().charAt(0)-96;
+        while ((row < 1 || row > mapSize) || (column < 1 || column > mapSize)) {
+            LOGGER.info("Invalid move! Please enter your move's placement (row, column):");
+            row = scanner.nextInt();
+            column = scanner.next().charAt(0)-96;
+        }
+        setMove(moves, 'x', row, column);
     }
 }
