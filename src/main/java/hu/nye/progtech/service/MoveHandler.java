71073c697cf47ea40f8a215ebb36277022854c58
Juhász Benedek <juhaszbeni51@gmail.com>
2025-10-16 21:03:38 +0200

    Added functionality to check if a move is legal.

--- src/main/java/hu/nye/progtech/service/MoveHandler.java
+++ src/main/java/hu/nye/progtech/service/MoveHandler.java
@@ -16,14 +16,115 @@ public class MoveHandler {
     public static void readMove(char[][] moves, int mapSize) {
         int row;
         int column;
+
         LOGGER.info("Please enter your move's placement (row, column):");
         row = scanner.nextInt();
         column = scanner.next().charAt(0) - 96;
-        while ((row < 1 || row > mapSize) || (column < 1 || column > mapSize)) {
-            LOGGER.info("Invalid move! Please enter your move's placement (row, column):");
-            row = scanner.nextInt();
-            column = scanner.next().charAt(0) - 96;
+
+        boolean movePlaced = false;
+        while (!(movePlaced)) {
+            if (isIllegal(mapSize, row, column)) {
+                LOGGER.info("Invalid move! (row, column):");
+                row = scanner.nextInt();
+                column = scanner.next().charAt(0) - 96;
+                continue;
+            } else if (isOccupied(moves, row, column)) {
+                LOGGER.info("Already occupied! (row, column):");
+                row = scanner.nextInt();
+                column = scanner.next().charAt(0) - 96;
+                continue;
+            } else if (!(isConnected(moves, mapSize, row, column))) {
+                LOGGER.info("Not connected! (row, column):");
+                row = scanner.nextInt();
+                column = scanner.next().charAt(0) - 96;
+                continue;
+            }
+            movePlaced = true;
         }
         setMove(moves, 'x', row, column);
+    }
+
+    public static boolean isIllegal(int mapSize, int row, int column) {
+        return row < 1 || row > mapSize || column < 1 || column > mapSize;
+    }
+
+    public static boolean isOccupied(char[][] moves, int row, int column) {
+        return moves[row - 1][column - 1] != ' ';
+    }
+
+    public static boolean isConnected(char[][] moves, int mapSize, int row, int column) {
+        // corner checks
+        if (row == 1 && column == 1) {
+            return moves[row - 1][column] != ' ' || moves[row][column - 1] != ' ' || moves[row][column] != ' ';
+        }
+        if (row == 1 && column == mapSize) {
+            return moves[row - 1][column - 2] != ' ' || moves[row][column - 2] != ' ' || moves[row][column - 1] != ' ';
+        }
+        if (row == mapSize && column == 1) {
+            return moves[row - 2][column - 1] != ' ' || moves[row - 2][column] != ' ' || moves[row - 1][column] != ' ';
+        }
+        if (row == mapSize && column == mapSize) {
+            return moves[row - 2][column - 2] != ' ' || moves[row - 2][column - 1] != ' ' || moves[row - 1][column - 2] != ' ';
+        }
+
+        // side checks
+        if (row == 1 && (column > 1 && column < mapSize)) {
+            if (moves[row - 1][column - 2] != ' ' || moves [row - 1][column] != ' ') {
+                return true;
+            } else {
+                for (int i = 2; i >= 0; i--) {
+                    if (moves[row][column - i] != ' ') {
+                        return true;
+                    }
+                }
+            }
+        }
+        if (row == mapSize && (column > 1 && column < mapSize)) {
+            if (moves[row - 1][column - 2] != ' ' || moves [row - 1][column] != ' ') {
+                return true;
+            } else {
+                for (int i = 2; i >= 0; i--) {
+                    if (moves[row - 2][column - i] != ' ') {
+                        return true;
+                    }
+                }
+            }
+        }
+        if (column == 1 && (row > 1 && row < mapSize)) {
+            if (moves[row - 2][column - 1] != ' ' || moves [row][column - 1] != ' ') {
+                return true;
+            } else {
+                for (int i = 2; i >= 0; i--) {
+                    if (moves[row - i][column] != ' ') {
+                        return true;
+                    }
+                }
+            }
+        }
+        if (column == mapSize && (row > 1 && row < mapSize)) {
+            if (moves[row - 2][column - 1] != ' ' || moves [row][column - 1] != ' ') {
+                return true;
+            } else {
+                for (int i = 2; i >= 0; i--) {
+                    if (moves[row - i][column - 2] != ' ') {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        //middle check
+        if ((row > 1 && row < mapSize) && (column > 1 && column < mapSize)) {
+            for (int i = 2; i >= 0; i--) {
+                if (moves[row - 2][column - i] != ' ') {
+                    return true;
+                }
+                if (moves[row][column - i] != ' ') {
+                    return true;
+                }
+            }
+            return moves[row - 1][column - 2] != ' ' || moves[row - 1][column] != ' ';
+        }
+        return false;
     }
 }
